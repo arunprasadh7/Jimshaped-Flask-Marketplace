0f01558d9f40b4cf267ccb99a9267ee1ec209a4e
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-01-17 05:09:45 +0530

    Styling and Templates

--- market.py
+++ market.py
@@ -1,14 +1,12 @@
-from flask import Flask 
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return '<h1>Hello World!!</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>Hello Welcome to {username}\'s About Page</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
